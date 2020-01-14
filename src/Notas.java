96abc6d8efddefa997a8f7353e4de54906f93605
GiihALves <gislaaineelaves@gmail.com>
2020-01-14 15:40:00 -0300

    Adicionado nota 4

---
+++ src/Notas.java
@@ -0,0 +1,21 @@
+public class Notas {
+    public static void main(String[] args) {
+        float nota1 = 4.6f;
+        float nota2 = 6.1f;
+        float nota3 = 1.9f;
+        double media = (nota1 + nota2 + nota3)/2;
+
+        if (media >= 6.5){
+            System.out.println("Aprovado");
+        }
+        else if (media > 5.0 && media < 6.5){
+            System.out.println("Recuperação");
+            if (media > 5.2 && media < 6.3){
+                System.out.println("Vai ter que estudar");
+            }
+        }
+        else{
+            System.out.println("Reprovado");
+        }
+    }
+}
